0f7ae9454a6635a4912410befe4fc39cd6d2c8e1
mediacloner <alexbcn@gmail.com>
2018-03-26 14:24:32 +0200

    update

--- api-client/dist/index.js
+++ api-client/dist/index.js
@@ -1,58 +1,63 @@
 'use strict';
 
-/* require('dotenv').config(); */
+require('dotenv').config();
 var axios = require('axios');
 var url = require('url');
 
 var apiClient = {
-    /*  baseUrl: ()=>{
-         return `${this.protocol}://${this.host}:${this.port}/api/`
-     }, */
-       baseUrl: `http://localhost:5000/api/`,
+    baseUrl: function baseUrl() {
+       /*  console.log('this->', undefined); */
+        return 'http://localhost:5000/api/'; //`${this.protocol}://${this.host}:${this.port}/api/`
+    },
+    //  baseUrl: `http://localhost:5000/api/`,
+    //baseUrl: `${this.protocol}://${this.host}:${this.port}/api/`,
 
-/*     baseUrl: "https://cryptic-bayou-64395.herokuapp.com/api/",
- */
+    /* baseUrl: "https://cryptic-bayou-64395.herokuapp.com/api/", */
+
     /*  
        login(username, password) {
            return this._call('post', 'login', { username, password })
        } */
 
     listPosts: function listPosts() {
-        return data(axios.get(url.resolve(this.baseUrl, 'list')));
+        return data(axios.get(url.resolve(undefined.baseUrl(), 'list')));
     },
 
     listPostsByUser: function listPostsByUser(id) {
-        return data(axios.get(url.resolve(this.baseUrl, 'list/' + id)));
+        return data(axios.get(url.resolve(this.baseUrl(), 'list/' + id)));
+    },
+    listFollowingByUser: function listFollowingByUser(id) {
+        return data(axios.get(url.resolve(this.baseUrl(), 'following/' + id)));
     },
     listPostsByGroup: function listPostsByGroup(id) {
-        return data(axios.get(url.resolve(this.baseUrl, 'listgroup/' + id)));
+        return data(axios.get(url.resolve(this.baseUrl(), 'listgroup/' + id)));
     },
     search: function search(word) {
-        return data(axios.get(url.resolve(this.baseUrl, 'search/' + word)));
+        return data(axios.get(url.resolve(this.baseUrl(), 'search/' + word)));
     },
     addKudo: function addKudo(id) {
-        return data(axios.put(url.resolve(this.baseUrl, 'kudos/' + id)));
+        return data(axios.put(url.resolve(this.baseUrl(), 'kudos/' + id)));
     },
     retrievePost: function retrievePost(id) {
-        return data(axios.get(url.resolve(this.baseUrl, 'post/' + id)));
+        return data(axios.get(url.resolve(this.baseUrl(), 'post/' + id)));
     },
     retrieveUser: function retrieveUser(id) {
-        return data(axios.get(url.resolve(this.baseUrl, 'user/' + id)));
+        return data(axios.get(url.resolve(this.baseUrl(), 'user/' + id)));
     },
     createComment: function createComment(id, userId, comment) {
         // id == post
-        return data(axios.post(url.resolve(this.baseUrl, 'comment/' + id), {
+        return data(axios.post(url.resolve(this.baseUrl(), 'comment/' + id), {
             userId: userId, comment: comment
         }));
     },
     createPost: function createPost(title, shortDescription, fullDescription, owner, idPostTemplate, namePostTemplate, tag, URLpath, time) {
-        return data(axios.post(url.resolve(this.baseUrl, 'post'), { title: title, shortDescription: shortDescription, fullDescription: fullDescription, owner: owner, idPostTemplate: idPostTemplate, namePostTemplate: namePostTemplate, tag: tag, URLpath: URLpath, time: time }));
+        return data(axios.post(url.resolve(this.baseUrl(), 'post'), { title: title, shortDescription: shortDescription, fullDescription: fullDescription, owner: owner, idPostTemplate: idPostTemplate, namePostTemplate: namePostTemplate, tag: tag, URLpath: URLpath, time: time }));
     },
     createUser: function createUser(name, surname, email, username, password, city, country, about, timelineTitle) {
-        return data(axios.post(url.resolve(this.baseUrl, 'user'), { name: name, surname: surname, email: email, username: username, password: password, city: city, country: country, about: about, timelineTitle: timelineTitle }));
+        return data(axios.post(url.resolve(this.baseUrl(), 'user'), { name: name, surname: surname, email: email, username: username, password: password, city: city, country: country, about: about, timelineTitle: timelineTitle }));
     },
     deleteComment: function deleteComment(id) {
-        return data(axios.delete(url.resolve(this.baseUrl, 'comment/' + id)));
+        return data(axios.delete(url.resolve(this.baseUrl(), 'comment/' + id)));
     }
 };
 
--- api-client/src/index.js
+++ api-client/src/index.js
@@ -3,10 +3,12 @@ const axios = require('axios')
 const url = require('url')
 
 const apiClient = {
-   /*  baseUrl: ()=>{
-        return `${this.protocol}://${this.host}:${this.port}/api/`
-    }, */
-   baseUrl: `http://localhost:5000/api/`,
+    baseUrl: ()=>{
+        console.log('this->',this)
+        return `http://localhost:5000/api/`//`${this.protocol}://${this.host}:${this.port}/api/`
+    }, 
+ //  baseUrl: `http://localhost:5000/api/`,
+ //baseUrl: `${this.protocol}://${this.host}:${this.port}/api/`,
 
 /* baseUrl: "https://cryptic-bayou-64395.herokuapp.com/api/", */
 
@@ -16,48 +18,53 @@ const apiClient = {
     } */
     
     listPosts: ()=> {
-        return data(axios.get(url.resolve(this.baseUrl, 'list')))
+        return data(axios.get(url.resolve(this.baseUrl(), 'list')))
     },
 
     listPostsByUser(id) {
-        return data(axios.get(url.resolve(this.baseUrl, `list/${id}`)))
+        return data(axios.get(url.resolve(this.baseUrl(), `list/${id}`)))
+ 
+    },
+
+    listFollowingByUser(id) {
+        return data(axios.get(url.resolve(this.baseUrl(), `following/${id}`)))
  
     },
     listPostsByGroup(id) {
-        return data(axios.get(url.resolve(this.baseUrl, `listgroup/${id}`))) 
+        return data(axios.get(url.resolve(this.baseUrl(), `listgroup/${id}`))) 
     },
     search(word) {
-        return data(axios.get(url.resolve(this.baseUrl, `search/${word}`)))
+        return data(axios.get(url.resolve(this.baseUrl(), `search/${word}`)))
     },
   
     addKudo (id) {
-        return data(axios.put(url.resolve(this.baseUrl, `kudos/${id}`)))
+        return data(axios.put(url.resolve(this.baseUrl(), `kudos/${id}`)))
     },
     retrievePost (id) {
-        return data(axios.get(url.resolve(this.baseUrl, `post/${id}`)))
+        return data(axios.get(url.resolve(this.baseUrl(), `post/${id}`)))
     },
     retrieveUser (id) {
-        return data(axios.get(url.resolve(this.baseUrl, `user/${id}`)))
+        return data(axios.get(url.resolve(this.baseUrl(), `user/${id}`)))
     },
 
 
 
     createComment ( id, userId, comment) {  // id == post
-        return data(axios.post(url.resolve(this.baseUrl, `comment/${id}`), {
+        return data(axios.post(url.resolve(this.baseUrl(), `comment/${id}`), {
             userId, comment
          })) 
     },
 
     createPost(  title, shortDescription, fullDescription, owner,idPostTemplate,namePostTemplate,tag, URLpath, time) {
-        return data(axios.post(url.resolve(this.baseUrl, 'post'), {  title,shortDescription,fullDescription,owner,idPostTemplate,namePostTemplate,tag, URLpath, time }))
+        return data(axios.post(url.resolve(this.baseUrl(), 'post'), {  title,shortDescription,fullDescription,owner,idPostTemplate,namePostTemplate,tag, URLpath, time }))
     },
 
     createUser ( name, surname, email, username, password, city, country, about, timelineTitle ) {
-        return data(axios.post(url.resolve(this.baseUrl, 'user'), { name, surname, email, username, password, city, country, about, timelineTitle  }))
+        return data(axios.post(url.resolve(this.baseUrl(), 'user'), { name, surname, email, username, password, city, country, about, timelineTitle  }))
     },
 
     deleteComment ( id ){
-        return data(axios.delete(url.resolve(this.baseUrl, `comment/${id}`)))
+        return data(axios.delete(url.resolve(this.baseUrl(), `comment/${id}`)))
     }
 
 }
